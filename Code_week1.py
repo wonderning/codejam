1cdb6b7ee69ff32ccef3606cbff54622e69006dd
Ningverse <wonderning@gmail.com>
2017-03-28 10:25:15 +0800

    Update Code_week1.py

--- Code_week1.py
+++ Code_week1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8
 # Author: Derek Wang
 # This is verified in Python 3.6.
 
@@ -8,21 +8,18 @@ import re
 # limit of total children
 COUNT_LIST = [i for i in range(2, 11)]
 # Regular expression for string input
-PAT = r'^("[a-zA-Z1-9]{1,50}",)*"[a-zA-Z1-9]{1,50}"$'
+PAT = r'^("[a-zA-Z\d]{1,50}",){0,49}"[a-zA-Z\d]{1,50}"$'
 
 
 def string2count(string, num):
     num = int(num)
-    (child_get, total_remain) = (0, 0)
-    dict = {}
-    string_list = re.split(',', string)
-    for str in string_list:
-        str = str.strip('"')
-        for letter in str:
-            if letter in dict:
-                dict[letter] += 1
-            else:
-                dict[letter] = 1
+    (child_get, total_remain, dict) = (0, 0, {})
+    string_list = re.sub('[,"]', "", string)
+    for letter in string_list:
+        if letter in dict:
+            dict[letter] += 1
+        else:
+            dict[letter] = 1
     for letter in dict:
         child_get += dict[letter] // num
         total_remain += dict[letter] % num
@@ -35,4 +32,4 @@ if __name__ == '__main__':
     if re.match(PAT, new_string) and child_count.isdigit() and int(child_count) in COUNT_LIST:
         string2count(new_string, child_count)
     else:
-        exit("-1,-1")
+        print("-1,-1")
